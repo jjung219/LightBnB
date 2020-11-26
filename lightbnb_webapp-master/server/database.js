d85e26a4ddc43e4ab526540bbc74ca79f96d33fe
Jennifer Jung <jenjung2194@gmail.com>
2020-11-26 19:56:34 +0000

    update getAllReservations function with sql queries

--- lightbnb_webapp-master/server/database.js
+++ lightbnb_webapp-master/server/database.js
@@ -84,7 +84,25 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  console.log('getting all reservations..');
+  const queryString = (`
+    SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    AND end_date < now()::date
+    GROUP BY reservations.id, properties.id
+    ORDER BY start_date
+    LIMIT $2;
+  `);
+
+  return client
+    .query(queryString, [guest_id, limit])
+    .then(res => res.rows)
+    .catch(err => console.log('query error', err.stack));
+
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
